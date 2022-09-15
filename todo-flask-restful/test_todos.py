3b2f267d8b8541568777122ea7e4946887ae36dc
Saurav <spike.samantray@gmail.com>
2022-09-15 08:42:25 +0530

    day1 update

--- todo-flask-restful/test_todos.py
+++ todo-flask-restful/test_todos.py
@@ -1,5 +1,5 @@
 import unittest
-import app
+import app3 as app
 
 unittest.TestLoader.sortTestMethodsUsing = None
 BASE_URL = '/api/todos'
@@ -9,16 +9,19 @@ class ToDosTest(unittest.TestCase):
         self.app = app.app
         self.app.testing = True
         self.client = self.app.test_client()
-        self.payload = {"id": 3, "name": "File ITR", "status": "COMPLETE"}
 
     def test_get_todos(self):
         response = self.client.get(BASE_URL)
         self.assertEqual(response.status_code, 200)
 
-    def test_create_todos(self):
-        response = self.client.post(BASE_URL, json = self.payload)
+    def test_create_todos_success(self):
+        response = self.client.post(BASE_URL, json = {"id": 3, "name": "Complete Python Basics", "status": "COMPLETE"})
         self.assertEqual(response.status_code, 201)
         self.assertEqual(len(response.json), 3)
+
+    def test_create_todos_error(self):
+        response = self.client.post(BASE_URL, json = {"id": 3, "name": "File ITR", "status": "COMPLETE"})
+        self.assertEqual(response.status_code, 400)     
 
 if __name__ == "__main__":
     unittest.main() 
